b0a32b129d24cdce4d69ec008debb82583b281d3
tammyhu22 <tammyhu@uw.edu>
2023-03-07 15:23:58 -0800

    no scrolling

--- init.js
+++ init.js
@@ -1,5 +1,10 @@
 (function() {
     console.log("It's working!")
+    window.addEventListener("keydown", function(e) {
+        if(["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"].indexOf(e.code) > -1) {
+            e.preventDefault();
+        }
+    }, false);
     const overworld = new Overworld({
         element: document.querySelector(".game-container")
     });
